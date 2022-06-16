4866ac4ceafe25cbc5e73ba165f95282cb67c4b5
Dristanta Silwal <you@example.com>
2022-06-16 15:06:26 +0545

    Updates

---
+++ precache-manifest.53bafb6a3ea1161224bd6053f923a4a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "716053c443609e04e3bd",
+    "url": "/pomodoro-clock/static/css/main.2cef4b06.chunk.css"
+  },
+  {
+    "revision": "716053c443609e04e3bd",
+    "url": "/pomodoro-clock/static/js/main.716053c4.chunk.js"
+  },
+  {
+    "revision": "48dc1c1c87d8faf3dbc4",
+    "url": "/pomodoro-clock/static/js/1.48dc1c1c.chunk.js"
+  },
+  {
+    "revision": "f41d346ce9ebc121a229",
+    "url": "/pomodoro-clock/static/js/runtime~main.f41d346c.js"
+  },
+  {
+    "revision": "ef0ee26242e775acc95a4a6e7a1599e8",
+    "url": "/pomodoro-clock/index.html"
+  }
+];
